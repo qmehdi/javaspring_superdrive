4a550e8514a3c0d4edbacc391719ddfdf7ab8dfb
Qamber Mehdi <qmehdi110@gmail.com>
2020-11-14 00:27:22 -0600

    Able to store and display Encrypted passwords in credentials tab

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
@@ -3,6 +3,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,18 +11,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @Controller
 public class CredentialController {
 
     private final CredentialService credentialService;
     private final UserService userService;
+    private final EncryptionService encryptionService;
     private Authentication authentication;
 
     // Constructor
-    public CredentialController(CredentialService credentialService, UserService userService) {
+    public CredentialController(CredentialService credentialService, UserService userService, EncryptionService encryptionService) {
 
         this.credentialService = credentialService;
         this.userService = userService;
+        this.encryptionService = encryptionService;
     }
 
     public User getLoggedInUserObject() {
@@ -39,8 +45,15 @@ public class CredentialController {
     @PostMapping("/credential-create")
     public String createCredential(@ModelAttribute("newCredentialmsg") Credential credentialForm, Model model) {
 
-        // hardcoding key to xyz for now
-        Credential userSubmittedCredential = new Credential(credentialForm.getUrl(), credentialForm.getUsername(), "xyz", credentialForm.getPassword(), getLoggedInUserObject().getUserId());
+        // Encrypt the password
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credentialForm.getPassword(), encodedKey);
+
+        // Create the credential object using the form fields, encodedKey, and encryptedPassword
+        Credential userSubmittedCredential = new Credential(credentialForm.getUrl(), credentialForm.getUsername(), encodedKey, encryptedPassword, getLoggedInUserObject().getUserId());
 
         credentialService.insertCredentialIntoDB(userSubmittedCredential);
 
