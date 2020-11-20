8b362ff58c52f9bf9d99350f128c0bfff8a2be6e
Qamber Mehdi <qmehdi110@gmail.com>
2020-11-20 15:49:46 -0600

    tmp signup using page class working

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -19,7 +19,6 @@ class CloudStorageApplicationTests {
 
 	@BeforeAll
 	static void beforeAll() {
-
 		WebDriverManager.chromedriver().setup();
 	}
 
@@ -35,38 +34,58 @@ class CloudStorageApplicationTests {
 		}
 	}
 
-	// Sign up and Login
 	@Test
-	public void signUpAndLoginTest() throws InterruptedException {
+	public void testUserSignupAndLogin() throws InterruptedException{
+		String _firstName = "John";
+		String _lastName = "Doe";
+		String _username = "jdoe";
+		String _password = "test123!";
 
 		driver.get("http://localhost:" + this.port + "/signup");
 
 		// ########### Sign Up ############
-		WebElement inputFieldFirstName = driver.findElement(By.id("inputFirstName"));
-		inputFieldFirstName.sendKeys("john");
+		SignUpPage signUpPage = new SignUpPage(driver);
 
-		WebElement inputFieldLastName = driver.findElement(By.id("inputLastName"));
-		inputFieldLastName.sendKeys("doe");
+		signUpPage.performSignUp(_firstName, _lastName, _username, _password);
 
-		WebElement inputFieldUsername = driver.findElement(By.id("inputUsername"));
-		inputFieldUsername.sendKeys("jdoe");
-
-		WebElement inputFieldPassword = driver.findElement(By.id("inputPassword"));
-		inputFieldPassword.sendKeys("test123!");
-
-		inputFieldPassword.submit();
-
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 
 		// ########### Login ##############
- 		WebElement inputFieldLoginUser = driver.findElement(By.id("inputUsername"));
-		inputFieldLoginUser.sendKeys("jdoe");
-
-		WebElement inputFieldLoginPassword = driver.findElement(By.id("inputPassword"));
-		inputFieldLoginPassword.sendKeys("test123!");
-
-		inputFieldLoginPassword.submit();
-
-		Thread.sleep(3000);
+		
 	}
+
+//	// Sign up and Login
+//	@Test
+//	public void signUpAndLoginTest() throws InterruptedException {
+//
+//		driver.get("http://localhost:" + this.port + "/signup");
+//
+//		// ########### Sign Up ############
+//		WebElement inputFieldFirstName = driver.findElement(By.id("inputFirstName"));
+//		inputFieldFirstName.sendKeys("john");
+//
+//		WebElement inputFieldLastName = driver.findElement(By.id("inputLastName"));
+//		inputFieldLastName.sendKeys("doe");
+//
+//		WebElement inputFieldUsername = driver.findElement(By.id("inputUsername"));
+//		inputFieldUsername.sendKeys("jdoe");
+//
+//		WebElement inputFieldPassword = driver.findElement(By.id("inputPassword"));
+//		inputFieldPassword.sendKeys("test123!");
+//
+//		inputFieldPassword.submit();
+//
+//		Thread.sleep(2000);
+//
+//		// ########### Login ##############
+// 		WebElement inputFieldLoginUser = driver.findElement(By.id("inputUsername"));
+//		inputFieldLoginUser.sendKeys("jdoe");
+//
+//		WebElement inputFieldLoginPassword = driver.findElement(By.id("inputPassword"));
+//		inputFieldLoginPassword.sendKeys("test123!");
+//
+//		inputFieldLoginPassword.submit();
+//
+//		Thread.sleep(3000);
+//	}
 }
---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/SignUpPage.java
@@ -0,0 +1,37 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignUpPage {
+
+    @FindBy(id = "inputFirstName")
+    private WebElement inputFirstName;
+
+    @FindBy(id = "inputLastName")
+    private WebElement inputLastName;
+
+    @FindBy(id = "inputUsername")
+    private WebElement inputUsername;
+
+    @FindBy(id = "inputPassword")
+    private WebElement inputPassword;
+
+    @FindBy(id = "submitButton")
+    private WebElement submitButton;
+
+    // Constructor
+    public SignUpPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void performSignUp(String firstName, String lastName, String username, String password) {
+        this.inputFirstName.sendKeys(firstName);
+        this.inputLastName.sendKeys(lastName);
+        this.inputUsername.sendKeys(username);
+        this.inputPassword.sendKeys(password);
+        this.submitButton.click();
+    }
+}
