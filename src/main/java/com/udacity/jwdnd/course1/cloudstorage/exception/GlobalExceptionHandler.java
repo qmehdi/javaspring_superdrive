b64c0170e6c12650631da3bf268d9b3c0272cc84
Qamber Mehdi <qmehdi110@gmail.com>
2020-11-22 19:39:27 -0600

    Added exception for fileupload SizeLimitExceededException

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.course1.cloudstorage.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //CommonsMultipartResolver
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleError2(MaxUploadSizeExceededException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("fileSizeExceededE", e.getCause().getMessage());
+        return "redirect:/home";
+
+    }
+}
